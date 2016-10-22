1bd692a09ac6bda3f9a6ff9310c320f501c153be
kb-studios <2015kaushal.bhogale@ves.ac.in>
2016-10-22 22:43:05 +0530

    Beta Version 	- The first Beta Version of the project!

--- main.c
+++ main.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <conio.h>
+#include <windows.h>
+
+/**Function Declarations*/
+void createAccount();
+int checkEmailValidity(char name[50]);
+//void addToUserData(struct UserData * temp);
+//void addToUserGraph(struct UserData * temp);
+void credRefresh();
+void credLoad();
+void displayUserData();
+void viewGraph();
+void storeGraph();
+void readGraph();
+void Login(char name[50]);
+void makeConnection(char name[50]);
+
 
 /**Global Variable Declarations**/
 int CurrentUserIndex = 0;
 int UserCounter = 1;
 struct UserData * startUser = NULL;
 struct User * startGraph = NULL;
-struct User * currentUserPointer = NULL;
+struct UserData * currentUserPointer = NULL;
 
 /*
 * This structure stores all the data of a user
@@ -17,9 +35,7 @@ struct UserData
 {
     int sysID;
     char name[50];
-    char dob[10];
     char password[20];
-    char status[140];
     struct UserData * next;
 };
 
@@ -46,7 +62,7 @@ struct Friend
 void createAccount()
 {
     struct UserData * temp, * ptr;
-    char password[20],c;
+    char password[20],passwordRepeat[20],c;
     int index = 0;
     char name[50];
 
@@ -85,6 +101,8 @@ void createAccount()
         }
         password[index] = '\0';
 
+        //printf("%s",password);
+
         index = 0;
 
         printf("\nRe-Enter Password : ");
@@ -100,32 +118,39 @@ void createAccount()
                index--;
            continue;
            }
-           temp->password[index++] = c;
+           passwordRepeat[index++] = c;
            putch('*');
         }
-        temp->password[index] = '\0';
+        passwordRepeat[index] = '\0';
 
-        if(strcmp(temp->password,password)==0)
+        //printf("%s",temp->password);
+
+        if(strcmp(passwordRepeat,password)==0)
+        {
+            strcpy(temp->password,password);
             break;
+        }
         else
             printf("\nPasswords did not match. Please enter again.");
     }
 
     //-------------- Entering Bio Data --------------//
+    /*
     printf("\n* Enter your BioData *");
+    printf("\nEnter your NickName (less than 7 characters) : ");
+    scanf("%s",temp->nickName);
 
-
-    printf("\nEnter you Date of Birth (YYYY-MM-DD): ");
-    scanf("%s",temp->dob);
+    printf("\nEnter your Age : ");
+    scanf("%d",&temp->age);
 
     fflush(stdin);
-    printf("\nEnter a status : (140 characters)");
-    scanf ("%[^\n]%*c", temp->status);
+    printf("\nEnter your Contact Number : ");
+    scanf ("%d",&temp->contact);*/
 
     addToUserData(temp);
     addToUserGraph(temp);
 
-    fileRefresh();
+    credRefresh();
 
     printf("\nAccount Created Successfully!");
 }
@@ -205,12 +230,12 @@ void addToUserGraph(struct UserData * temp)
     }
 }
 
-void fileRefresh()
+void credRefresh()
 {
     FILE *fp;
     struct UserData * ptr;
 
-    if((fp = fopen("User_Data.kbs","wb"))==NULL)
+    if((fp = fopen("UserCredentials.kbs","wb"))==NULL)
     {
         printf("\nFile Error");
         return;
@@ -220,13 +245,38 @@ void fileRefresh()
 
     while(ptr != NULL)
     {
-        fwrite(ptr,sizeof(struct UserData),1,fp);
+        fwrite(ptr->name,50*sizeof(char),1,fp);
+        fwrite(ptr->password,20*sizeof(char),1,fp);
         ptr = ptr->next;
     }
     fclose(fp);
 }
 
-void fileLoad()
+/*
+void bioRefresh()
+{
+    FILE *fp;
+    struct UserData * ptr;
+
+    if((fp = fopen("UserBio.kbs","wb"))==NULL)
+    {
+        printf("\nFile Error");
+        return;
+    }
+
+    ptr = startUser;
+
+    while(ptr != NULL)
+    {
+        fwrite(ptr->age,sizeof(int),1,fp);
+        fwrite(ptr->contact,sizeof(int),1,fp);
+        fwrite(ptr->password,6*sizeof(char),1,fp);
+        ptr = ptr->next;
+    }
+    fclose(fp);
+}*/
+
+void credLoad()
 {
     FILE *fp;
     struct UserData * ptr;
@@ -234,14 +284,14 @@ void fileLoad()
 
     ptr = (struct UserData * )malloc(sizeof(struct UserData));
 
-    if((fp = fopen("User_Data.kbs","rb"))==NULL)
+    if((fp = fopen("UserCredentials.kbs","rb"))==NULL)
     {
         printf("\nFile Error");
         return;
     }
 
-    fread(ptr,sizeof(struct UserData),1,fp);
-
+    fread(ptr->name,50*sizeof(char),1,fp);
+    fread(ptr->password,20*sizeof(char),1,fp);
 
     while(1)
     {
@@ -249,11 +299,35 @@ void fileLoad()
         addToUserData(ptr);
         addToUserGraph(ptr);
         ptr = (struct UserData * )malloc(sizeof(struct UserData));
-        if(!(fread(ptr,sizeof(struct UserData),1,fp)))
+        if(!(fread(ptr->name,50*sizeof(char),1,fp)) || !(fread(ptr->password,20*sizeof(char),1,fp)))
             break;
     }
     fclose(fp);
 }
+
+/*
+void bioLoad()
+{
+    FILE *fp;
+    struct UserData * ptr;
+
+    if((fp = fopen("UserBio.kbs","rb"))==NULL)
+    {
+        printf("\nFile Error");
+        return;
+    }
+
+    ptr = startUser;
+
+    while(ptr != NULL)
+    {
+        fread(ptr->age,sizeof(int),1,fp);
+        fread(ptr->contact,sizeof(int),1,fp);
+        fread(ptr->password,6*sizeof(char),1,fp);
+        ptr = ptr->next;
+    }
+    fclose(fp);
+}*/
 
 void displayUserData()
 {
@@ -269,6 +343,86 @@ void displayUserData()
     }
 }
 
+void viewGraph()
+{
+    struct User * ptrUser;
+    struct Friend * ptrFriend;
+
+    ptrUser = startGraph;
+
+    while(ptrUser!=NULL)
+    {
+        ptrFriend = ptrUser->startFriend;
+        printf("\n%s -> ",ptrUser->UserPointer->name);
+        while(ptrFriend!=NULL)
+        {
+            printf("%s, ",ptrFriend->Friend->name);
+            ptrFriend = ptrFriend->next;
+        }
+        ptrUser = ptrUser->next;
+    }
+}
+
+void storeGraph()
+{
+    struct User * ptrUser;
+    struct Friend * ptrFriend;
+    FILE * fp;
+
+    if((fp=fopen("Graph.txt","w"))==NULL)
+    {
+        printf("\nFile Error");
+    }
+
+    ptrUser = startGraph;
+
+    while(ptrUser!=NULL)
+    {
+        ptrFriend = ptrUser->startFriend;
+        fprintf(fp,"%s",ptrUser->UserPointer->name);
+        while(ptrFriend!=NULL)
+        {
+            fprintf(fp,"%s",ptrFriend->Friend->name);
+            ptrFriend = ptrFriend->next;
+        }
+        ptrUser = ptrUser->next;
+    }
+
+    fclose(fp);
+}
+
+void readGraph()
+{
+    struct User * ptrUser;
+    struct Friend * ptrFriend;
+    FILE * fp;
+
+    if((fp=fopen("Graph.txt","r"))==NULL)
+    {
+        printf("\nFile Error");
+    }
+
+    ptrUser = (struct User *)malloc(sizeof(struct User));
+
+    ptrUser = startGraph;
+
+    while(ptrUser!=NULL)
+    {
+        ptrFriend = ptrUser->startFriend;
+        fscanf(fp,"%s",ptrUser->UserPointer->name);
+        currentUserPointer = ptrUser->UserPointer;
+        while(ptrFriend!=NULL)
+        {
+            fscanf(fp,"%s",ptrFriend->Friend->name);
+            makeConnection(ptrFriend->Friend->name);
+        }
+
+        ptrUser = (struct User *)malloc(sizeof(struct User));
+        ptrUser = ptrUser->next;
+    }
+
+    currentUserPointer = NULL;
+}
 
 void Login(char name[50])
 {
@@ -280,7 +434,7 @@ void Login(char name[50])
 
     while(ptr != NULL)
     {
-        if(strcmp(ptr->name,name)==0)
+        if(strcmp(strupr(ptr->name),strupr(name))==0)
         {
             printf("\nEmail Id Obtained");
 
@@ -325,7 +479,7 @@ void Login(char name[50])
     }
 }
 
-void addFriend(struct UserData * UserPointer,struct UserData * friend)
+void addFriend(struct UserData * UserPointer,struct UserData * friend1)
 {
     struct User * ptr;
     struct Friend * temp;
@@ -342,7 +496,7 @@ void addFriend(struct UserData * UserPointer,struct UserData * friend)
         ptr = ptr->next;
     }
 
-    temp->Friend = friend;
+    temp->Friend = friend1;
     temp->next = NULL;
 
     if(ptr->startFriend == NULL)
@@ -447,10 +601,10 @@ void makeConnection(char name[50])
     addFriend(currentUserPointer,temp);
     addFriend(temp,currentUserPointer);
 
+    storeGraph();
     printf("\n* Connection Successful *\nFriend Added");
 
 }
-////////////////////////////////////////////////////
 
 void displayFriends(char name[50])
 {
@@ -474,9 +628,10 @@ void displayFriends(char name[50])
 
     ptr = temp->startFriend;
 
-    if(ptr = NULL)
+    if(ptr == NULL)
     {
         printf("\n%s has no Friends.",tempUser->name);
+        return;
     }
 
     printf("\nThe Friend List of %s is : ",tempUser->name);
@@ -488,11 +643,220 @@ void displayFriends(char name[50])
 
 }
 
+void headerLoad()
+{
+    FILE * fp;
+    char c;
+
+    if((fp=fopen("header.txt","r"))==NULL)
+    {
+        printf("\nFile Error");
+    }
+
+    while((c=fgetc(fp))!=EOF)
+    {
+        printf("%c",c);
+    }
+}
+
+void help()
+{
+    FILE * fp;
+
+    if((fp=fopen("Documentation.txt","r"))==NULL)
+    {
+        printf("\nFile Error");
+    }
+
+    readFile(fp);
+}
+
+void readFile(FILE * fp)
+{
+    char ch,c;
+
+    while((c=fgetc(fp))!=EOF)
+    {
+        if(c == '+')
+        {
+            printf("\nContinue reading?(c/q): ");
+            ch=getch();
+            if (ch=='c' || ch=='C')
+            {
+                continue;
+            }
+            else
+                break;
+
+        }
+        printf("%c",c);
+    }
+    fclose(fp);
+}
+
+void loadingScreen()
+{
+    char load[70]="|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\|/-\\\0";
+    int i;
+    printf("LOADING ");
+
+    for(i=0;load[i]!='\0';i++)
+    {
+        printf("%c",load[i]);
+        Sleep(120);
+        putch('\b');
+
+    }
+    Sleep(200);
+    system("cls");
+}
+
+/*
+void displayBio(char name[50])
+{
+
+    struct UserData * temp;
+
+    if(currentUserPointer == NULL)
+    {
+        printf("\n* Login Error *\nYou aren't Logged in! Please login with a valid userName!");
+        return;
+    }
+
+    if((temp=searchUserData(name))==NULL)
+    {
+        printf("\n* Username Error *\nInvalid Username!Please try again");
+        return;
+    }
+
+    printf("\n*****   %s's PROFILE   *****",temp->name);
+    printf("Hello Friends, I am popularly known as %s. I am %d years old.\n",temp->nickName,temp->age);
+    printf("Contact me on : %d",temp->contact);
+}*/
+
+void createPostFile()
+{
+    FILE * fp;
+
+    if((fp=fopen("ADD_POST.txt","w"))==NULL)
+    {
+        printf("\nFile Error");
+    }
+
+    fprintf(fp,"\n");
+    fprintf(fp,"\n++++++++++++++ Add the status Post in the above space ++++++++++");
+    fprintf(fp,"\n++ Your post can be of any length. No limitation on post size.");
+    fprintf(fp,"\n++ Anything written after the '+' sign will be ignored.");
+    fprintf(fp,"\n++ Make sure your Post does not have any plus sign, or else");
+    fprintf(fp,"\n++ even that will be ignored.");
+    fprintf(fp,"\n++ Save the File after the Post is ready.");
+    fprintf(fp,"\n++ Close notepad.exe after the Post will be Posted.");
+
+
+    fclose(fp);
+}
+
+void timestamp(FILE * fp)
+{
+    time_t ltime; /* calendar time */
+    ltime=time(NULL); /* get current cal time */
+    fprintf(fp,"%s",asctime( localtime(&ltime) ) );
+}
+
+void readPostFile()
+{
+    FILE * fpTimeTemp, * fpPost, *fpTime;
+    char c;
+
+    if((fpTimeTemp=fopen("TIMELINE_CPY.txt","w"))==NULL)
+    {
+        printf("\nFile Error");
+    }
+
+    if((fpPost=fopen("ADD_POST.txt","r"))==NULL)
+    {
+        printf("\nFile Error");
+    }
+
+    if((fpTime=fopen("TIMELINE.txt","r"))==NULL)
+    {
+        printf("\nFile Error");
+    }
+
+    while((c=fgetc(fpPost))!= EOF)
+    {
+        if(c=='+')
+            break;
+        fputc(c,fpTimeTemp);
+    }
+    fclose(fpPost);
+
+    fprintf(fpTimeTemp," --- Posted by %s on : ",currentUserPointer->name);
+    timestamp(fpTimeTemp);
+    fprintf(fpTimeTemp,"\n+\n");
+
+
+    while((c=fgetc(fpTime))!= EOF)
+    {
+        fputc(c,fpTimeTemp);
+    }
+    fclose(fpTime);
+    fclose(fpTimeTemp);
+
+    remove("TIMELINE.txt");
+    remove("ADD_POST.txt");
+    rename("TIMELINE_CPY.txt","TIMELINE.txt");
+}
+
+void addPost()
+{
+    if(currentUserPointer == NULL)
+    {
+        printf("\n* Login Error *\nYou aren't Logged in! Please login with a valid userName!");
+        return;
+    }
+
+    createPostFile();
+
+    (void)system("ADD_POST.txt");
+
+    readPostFile();
+
+    printf("\n* Post Successful *\nYou can view your post by >>>VIEW TIMELINE");
+}
+
+void viewTimeline()
+{
+    FILE * fp;
+
+    if(currentUserPointer == NULL)
+    {
+        printf("\n* Login Error *\nYou aren't Logged in! Please login with a valid userName!");
+        return;
+    }
+
+    if((fp=fopen("TIMELINE.txt","r"))==NULL)
+    {
+        printf("\nFile Error");
+    }
+
+    readFile(fp);
+}
+
+////////////////////////////////////////////////////
+
 void main()
 {
     char cmd1[10],cmd2[10], parameter[50];
 
-    fileLoad();
+    credLoad();
+    //readGraph();
+    loadingScreen();
+    headerLoad();
+
+    //(void)system("header.txt");
+
+    //readGraph();
 
     do
     {
@@ -501,18 +865,18 @@ void main()
         fflush(stdin);
         scanf("%s",cmd1);
 
-        if(strcmp(strupr(cmd1),"CREATE") == 0)
+        if(strcmp(strupr(cmd1),"SIGN") == 0)
         {
             scanf("%s",cmd2);
 
-            if(strcmp(strupr(cmd2),"ACCOUNT") == 0)
+            if(strcmp(strupr(cmd2),"UP") == 0)
             {
                 createAccount();
             }
 
             else
             {
-                printf("\n* Invalid Command *\nType 'HELP?' for Command Docs");
+                printf("\n* Invalid Command *\nType '?HELP' for Command Docs");
             }
         }
 
@@ -525,15 +889,20 @@ void main()
                 displayUserData();
             }
 
-            else if(strcmp(strupr(cmd2),"FRIENDS") == 0)
+            else if(strcmp(strupr(cmd2),"FRIENDLIST") == 0)
             {
                 scanf("%s",parameter);
                 displayFriends(parameter);
             }
 
+            else if(strcmp(strupr(cmd2),"TIMELINE") == 0)
+            {
+                viewTimeline();
+            }
+
             else
             {
-                printf("\n* Invalid Command *\nType 'HELP?' for Command Docs");
+                printf("\n* Invalid Command *\nType '?HELP' for Command Docs");
             }
         }
 
@@ -549,7 +918,7 @@ void main()
 
             else
             {
-                printf("\n* Invalid Command *\nType 'HELP?' for Command Docs");
+                printf("\n* Invalid Command *\nType '?HELP' for Command Docs");
             }
         }
 
@@ -563,23 +932,44 @@ void main()
                 makeConnection(parameter);
             }
 
+            else if(strcmp(strupr(cmd2),"POST") == 0)
+            {
+                addPost();
+            }
+
             else
             {
-                printf("\n* Invalid Command *\nType 'HELP?' for Command Docs");
+                printf("\n* Invalid Command *\nType '?HELP' for Command Docs");
             }
         }
 
-        if(strcmp(strupr(cmd1),"LOGOUT") == 0)
+        else if(strcmp(strupr(cmd1),"LOGOUT") == 0)
         {
             currentUserPointer = NULL;
+            printf("\n* Logout Successful *");
         }
 
+        else if(strcmp(strupr(cmd1),"?HELP") == 0)
+        {
+            help();
+        }
+
+        else if(strcmp(strupr(cmd1),"CLS") == 0)
+        {
+            system("cls");
+            headerLoad();
+        }
+
+        else if(strcmp(strupr(cmd1),"QUIT") == 0)
+        {
+            break;
+        }
 
         else
         {
-            printf("\n* Invalid Command *\nType 'HELP?' for Command Docs");
+            printf("\n* Invalid Command *\nType '?HELP' for Command Docs");
         }
 
 
-    }while(strcmp(strupr(cmd1),"QUIT") != 0);
+    }while(1);
 }
